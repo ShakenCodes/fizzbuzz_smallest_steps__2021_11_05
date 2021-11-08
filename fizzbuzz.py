3e3a1e1e66c6eb31e1239c7b5dc8eace7dfec050
pmarkgraf-bionaut <89817253+pmarkgraf-bionaut@users.noreply.github.com>
2021-11-08 09:14:36 -0800

    r Eliminates canned response

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,27 +1,13 @@
 # -*- coding: utf-8 -*-
+from unittest import result
+
+
 def fizzbuzz_thirty():
     "Returns the first thirty responses to fizzbuzz"
-    other_responses = \
-        "16\n" \
-        "17\n" \
-        "fizz\n" \
-        "19\n" \
-        "buzz\n" \
-        "fizz\n" \
-        "22\n" \
-        "23\n" \
-        "fizz\n" \
-        "buzz\n" \
-        "26\n" \
-        "fizz\n" \
-        "28\n" \
-        "29\n" \
-        "fizzbuzz\n"
-    calculated_response = ""
-    for x in range (1, 16):
-        calculated_response += fizzbuzz(x) + "\n"
-    thirty_responses = calculated_response + other_responses
-    return thirty_responses
+    result = ""
+    for x in range (1, 31):
+        result += fizzbuzz(x) + "\n"
+    return result
 
 def fizzbuzz(n):
     "Calculates fizzbuzz for a given value"
