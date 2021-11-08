13a87f5d45ce025c99c14718688c4b28a80e94af
pmarkgraf-bionaut <89817253+pmarkgraf-bionaut@users.noreply.github.com>
2021-11-08 09:12:23 -0800

    f Adds fizzbuzz support

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,16 +2,6 @@
 def fizzbuzz_thirty():
     "Returns the first thirty responses to fizzbuzz"
     other_responses = \
-        "fizz\n" \
-        "7\n" \
-        "8\n" \
-        "fizz\n" \
-        "buzz\n" \
-        "11\n" \
-        "fizz\n" \
-        "13\n" \
-        "14\n" \
-        "fizzbuzz\n" \
         "16\n" \
         "17\n" \
         "fizz\n" \
@@ -28,14 +18,16 @@ def fizzbuzz_thirty():
         "29\n" \
         "fizzbuzz\n"
     calculated_response = ""
-    for x in range (1, 6):
+    for x in range (1, 16):
         calculated_response += fizzbuzz(x) + "\n"
     thirty_responses = calculated_response + other_responses
     return thirty_responses
 
 def fizzbuzz(n):
     "Calculates fizzbuzz for a given value"
-    if n % 3 == 0:
+    if n % 15 == 0:
+        return "fizzbuzz"
+    elif n % 3 == 0:
         return "fizz"
     elif n % 5 == 0:
         return "buzz"
